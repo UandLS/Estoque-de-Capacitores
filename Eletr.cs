4626ad462a8050a6ccb3055100dd345a31be2559
Uanderson Lessa <126968855+UandLS@users.noreply.github.com>
2023-11-01 00:05:26 -0300

    Add files via upload

---
+++ Eletr.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Globalization;
+
+
+public class Eletr
+{
+    public string Cod;
+    public int Quant;
+    public double Cap;
+
+    public Eletr(string cod1, double capacit, int quant1)
+    {
+
+        Cod = cod1;
+        Quant = quant1;
+        Cap = capacit;
+
+    }
+    public void addestq(int addquant)
+    {
+        Quant += addquant;
+    }
+
+    public void removstq(int removquant)
+    {
+        Quant -= removquant;
+    }
+
+
+
+
+    public override String ToString()
+    {
+        return "Cap " + Cod + " " + Cap.ToString("F2", CultureInfo.InvariantCulture) + "uF "
+         + Quant + " peças";
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Globalization;
+
+namespace Estoque0
+{
+
+    public static class Program
+    {
+        public static void Main()
+        {
+
+            int add;
+            int remov = 0;
+
+
+            Console.Write("Digite o  codigo do componente: ");
+            string codig = Console.ReadLine();
+
+            Console.Write("Digite a capacitancia do componente: ");
+            double tancia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Digite a quantidade no estoque: ");
+            int quantcap = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            Eletr c = new Eletr(codig, tancia, quantcap);
+
+
+
+
+            Console.WriteLine(" " + c);
+
+            Console.WriteLine();
+            Console.Write(" adicionar estoque desse componente: ");
+            add = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            c.addestq(add);
+            Console.WriteLine("Estoque atualizado: " + c);
+
+
+            Console.WriteLine();
+            Console.Write(" remover estoque desse componente: ");
+            remov = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            c.removstq(remov);
+            Console.WriteLine("Estoque atualizado: " + c);
+
+        }
+    }
+}
